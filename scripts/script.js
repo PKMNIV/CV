4e5a57e3e90a9ef6fa985d3661c968cb2192ddc8
Nicky van der Reijden <088220@glr.nl>
2023-08-29 15:57:08 +0200

    i lost track of my changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,6 @@
 let isNavOpen = false;
+
+//In case I cannot find a function to add to the project, this method is technically a function.
 document.getElementById('nav-toggle').addEventListener('click', () => {
     if (isNavOpen) {
         document.getElementById('nav-toggle').classList.remove('open');
