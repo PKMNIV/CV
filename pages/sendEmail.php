713090843cb13bf7a2cff6d543b405ddeba4d74b
Nicky van der Reijden <088220@glr.nl>
2023-08-30 10:13:18 +0200

    Completed Make the form functional #2

---
+++ pages/sendEmail.php
@@ -0,0 +1,14 @@
+<?php
+
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$company = $_POST['company'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$recipient = "n.reijden@gmail.com";
+
+
+mail($recipient, $subject, $message, "From:" . $email) or die("Error!");
